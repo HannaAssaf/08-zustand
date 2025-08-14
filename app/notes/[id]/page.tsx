7a4100d62033d0147684e6a535e7a11a19bf2c1d
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-08-14 22:58:13 +0100

    test

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -10,29 +10,29 @@ type Props = {
   params: Promise<{ id: string }>;
 };
 
-export async function generateMetadata({ params }: Props) {
-  const { id } = await params;
-  const note = await fetchNoteById(id);
-  return {
-    title: `Note: ${note.title}`,
-    description: note.content.slice(0, 30),
-    openGraph: {
-      title: `Note: ${note.title}`,
-      description: note.content.slice(0, 100),
-      url: `https://notehub.com/notes/${id}`,
-      siteName: "NoteHub",
-      images: [
-        {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-          width: 1200,
-          height: 630,
-          alt: note.title,
-        },
-      ],
-      type: "article",
-    },
-  };
-}
+// export async function generateMetadata({ params }: Props) {
+//   const { id } = await params;
+//   const date = await fetchNoteById(id);
+//   return {
+//     title: `Note: ${date.title}`,
+//     description: date.content.slice(0, 30),
+//     openGraph: {
+//       title: `Note: ${date.title}`,
+//       description: date.content.slice(0, 100),
+//       url: `https://notehub.com/notes/${id}`,
+//       siteName: "NoteHub",
+//       images: [
+//         {
+//           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+//           width: 1200,
+//           height: 630,
+//           alt: date.title,
+//         },
+//       ],
+//       type: "article",
+//     },
+//   };
+// }
 
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
